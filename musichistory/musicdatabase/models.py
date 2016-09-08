f2cd91ed76080c4f194378b1affd62a40b8876c2
pbhowelljr <pbhowelljr@gmail.com>
2016-09-08 11:26:25 -0500

    add artist/album/song models

--- musichistory/musicdatabase/models.py
+++ musichistory/musicdatabase/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Artist(models.Model):
+    name = models.TextField()
+
+class Album(models.Model):
+    name = models.TextField()
+
+class Song(models.Model):
+    name = models.TextField()
